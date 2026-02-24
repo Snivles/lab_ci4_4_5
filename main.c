a8b92cf5ac5ade5cc44ecc540e9c00d1d61a7594
Golovin Kirill <kirill.golovin.256@gmail.com>
2026-02-24 11:05:19 +0700

    цикл для проверки строки на наличие букв
    цикл который смотрит строку с конца и проверяет что текст содержит только цифры

--- main.c
+++ main.c
@@ -3,6 +3,12 @@
 int main()
 {
     char s1[1000] = "00004";
+    size_t len = strlen(s1); // вводим длину s1
+
+    for (int i =len-1 ; i>=0;i--){
+        if(s1[i] < 48 || s1[i]> 57){
+            printf("Строчка не в десятичной сис");
+            return 0;}}
     printf("Hello World!\n");
     return 0;
 }
