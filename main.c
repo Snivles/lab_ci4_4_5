ec8f235d612b82e621d5a5b7019b1938699b3c5c
Golovin Kirill <kirill.golovin.256@gmail.com>
2026-02-24 11:08:50 +0700

    добавление переменных для работы со строкой и проверка длины строки
    добавляем финальную сумму и последний символ ( тк дана непустая строка) и при помощи знаний кратности чисел на 4 считаем count

--- main.c
+++ main.c
@@ -9,6 +9,14 @@ int main()
         if(s1[i] < 48 || s1[i]> 57){
             printf("Строчка не в десятичной сис");
             return 0;}}
+    int count = -1; // финальная сумма , -1 тк 0 кратен любому число
+    int posled = len-1; // последний символ
+
+
+    if (len>=2){
+          int predposled = len-2;
+          count = (s1[posled]-'0') + (s1[predposled]-'0')*10;}
+    else {count = s1[posled]-'0';}
     printf("Hello World!\n");
     return 0;
 }
