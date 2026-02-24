cc60e28019fac14bc3d9be102684f4494f611f2e
Golovin Kirill <kirill.golovin.256@gmail.com>
2026-02-24 10:50:45 +0700

    заполняем s3
    в s3 храним вхождения символов : 1 - входит / 0 - не входит

--- main.c
+++ main.c
@@ -5,7 +5,9 @@ int main()
     char s2[1000] = "delo";
     int  s3[256] = {0}; // работаем с ним
 
-
+    for (int i=0; s2[i]!='\0';i++){
+            s3[(unsigned char)s2[i]]=1;
+}
     printf("Hello World!\n");
     return 0;
 }
