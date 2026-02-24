c455f0f3c2896657bc37a80c871db1da7230e2e8
Golovin Kirill <kirill.golovin.256@gmail.com>
2026-02-24 10:48:05 +0700

    подключение библиотеки и строк с которыми работать

--- main.c
+++ main.c
@@ -1,7 +1,11 @@
 #include <stdio.h>
-
+#include <string.h>
 int main()
-{
+{   char s1[1000] = "Hello World!";
+    char s2[1000] = "delo";
+    int  s3[256] = {0}; // работаем с ним
+
+
     printf("Hello World!\n");
     return 0;
 }
