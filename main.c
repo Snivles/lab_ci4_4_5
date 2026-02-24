5b3de18584e8914ca733a2619c3120b11d3bbe44
Golovin Kirill <kirill.golovin.256@gmail.com>
2026-02-24 11:10:41 +0700

    добавляем вывод

--- main.c
+++ main.c
@@ -2,14 +2,14 @@
 #include <string.h>
 int main()
 {
-    char s1[1000] = "00004";
+    char s1[1000] = "4";
     size_t len = strlen(s1); // вводим длину s1
 
     for (int i =len-1 ; i>=0;i--){
         if(s1[i] < 48 || s1[i]> 57){
             printf("Строчка не в десятичной сис");
             return 0;}}
-    int count = -1; // финальная сумма , -1 тк 0 кратен любому число
+    int count = -1; // переменная на которую делим  , -1 тк 0 кратен любому число
     int posled = len-1; // последний символ
 
 
@@ -17,6 +17,10 @@ int main()
           int predposled = len-2;
           count = (s1[posled]-'0') + (s1[predposled]-'0')*10;}
     else {count = s1[posled]-'0';}
-    printf("Hello World!\n");
+
+    if (count%4==0){
+      printf("Да, тк число %d кратно 4 ",count);}
+    else{
+      printf("Нет, тк число %d не кратно 4 ",count);}
     return 0;
 }
