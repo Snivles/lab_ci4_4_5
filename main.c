7f53e3a4ce353e3eb717af3b2abed96a1b8a2750
Golovin Kirill <kirill.golovin.256@gmail.com>
2026-02-24 11:16:57 +0700

    подключение библиотеки и строк с которыми работать

--- main.c
+++ main.c
@@ -1,7 +1,13 @@
 #include <stdio.h>
-
+#include <string.h>
 int main()
 {
+    char s1[1000] = "abc abc dabc abcd";// строка в которой пропускаем
+    char w[1000] = "abc";// символ который пропускаем
+    char news1[1000] = "";
+
+
+
     printf("Hello World!\n");
     return 0;
 }
