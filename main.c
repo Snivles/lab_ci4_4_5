d09c2fd8e36dd932f656b7f9d0293fa3bda4288d
Golovin Kirill <kirill.golovin.256@gmail.com>
2026-02-24 10:53:30 +0700

    реализация основного цикла
    реализация цикла в котором идет уже перезапись строки s1 если символы есть в строке s2 при помощи s3

--- main.c
+++ main.c
@@ -6,7 +6,12 @@ int main()
     int  s3[256] = {0}; // работаем с ним
 
     for (int i=0; s2[i]!='\0';i++){
-            s3[(unsigned char)s2[i]]=1;
+            s3[(unsigned char)s2[i]]=1;}
+    int index = 0;
+    for(int i =0; s1[i] != '\0'; i++){
+            if (s3[(unsigned char)s1[i]]==0){
+                s1[index] = s1[i];
+                index++;}
 }
     printf("Hello World!\n");
     return 0;
