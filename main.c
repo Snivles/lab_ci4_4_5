b66df0b34d23442f31539815cb41f1056d998438
Golovin Kirill <kirill.golovin.256@gmail.com>
2026-02-28 22:37:18 +0700

    Переосмысление предыдущей реализации и сведения к общему виду
    Прошлая реализация работа только для чисел кратных 4 тк использовались свойства кратности чисел на 4 (последние 2 числа должны быть кратны 4 тогда и все число) поэтому для более общего случая я решил реализовать работу с остатками (тоже по правилам математики)

--- main.c
+++ main.c
@@ -2,25 +2,20 @@
 #include <string.h>
 int main()
 {
-    char s1[1000] = "4";
+    char s1[1000] = "125";
+    int chislo = 5;
     size_t len = strlen(s1); // вводим длину s1
-
     for (int i =len-1 ; i>=0;i--){
         if(s1[i] < 48 || s1[i]> 57){
             printf("Строчка не в десятичной сис");
-            return 0;}}
-    int count = -1; // переменная на которую делим  , -1 тк 0 кратен любому число
-    int posled = len-1; // последний символ
+            return 0;}  }
+    int ostat = 0;
+    for (int i = 0; i <(int)len; i++){
+        int delim = s1[i] - '0';
+        ostat = (10 * ostat + delim) % chislo;}
+    if (ostat == 0){
+      printf("Да число кратно 4 тк остаток равен %d",ostat);}
+    else{printf("Нет число не кратно тк остаток равкен %d",ostat);}
 
-
-    if (len>=2){
-          int predposled = len-2;
-          count = (s1[posled]-'0') + (s1[predposled]-'0')*10;}
-    else {count = s1[posled]-'0';}
-
-    if (count%4==0){
-      printf("Да, тк число %d кратно 4 ",count);}
-    else{
-      printf("Нет, тк число %d не кратно 4 ",count);}
     return 0;
 }
