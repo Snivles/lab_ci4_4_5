1b3df9974285e0037a27f6a840279ace629e6708
Golovin Kirill <kirill.golovin.256@gmail.com>
2026-02-28 21:31:51 +0700

    Реализация кода в функции Stringcompaction
    добавлена функция с типом вывода char* тк делается обращение ко всей строке и внутри функции выполняется основная часть кода

--- main.c
+++ main.c
@@ -1,19 +1,27 @@
 #include <stdio.h>
 #include <string.h>
-int main()
-{   char s1[1000] = "Hello World!";
-    char s2[1000] = "delo";
-    int  s3[256] = {0}; // работаем с ним
 
+char* Stringcompaction(char *s1, char *s2){
+    int s3[256] = {0};
     for (int i=0; s2[i]!='\0';i++){
             s3[(unsigned char)s2[i]]=1;}
     int index = 0;
     for(int i =0; s1[i] != '\0'; i++){
             if (s3[(unsigned char)s1[i]]==0){
                 s1[index] = s1[i];
-                index++;}
-}
-    s1[index] ='\0';
+                index++;}}
+    s1[index]='\0';
+    return s1;}
+
+
+
+
+int main()
+{   char s1[1000] = "Hello World!";
+    char s2[1000] = "l";
+
+
+    Stringcompaction(s1,s2);
     printf("Строка s1 без символов s2 имеет вид %s\n",s1);
     return 0;
 }
