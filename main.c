09bd426447b03e85a8cbf6beec184118fd130ff6
Golovin Kirill <kirill.golovin.256@gmail.com>
2026-02-28 23:07:55 +0700

    Вынос проверки на кратность в функцию Multipleofanumber
    Реализация алгоритма реализована в функции Multipleofanumber и возвращает остаток конечный который проверяется уже ( если остаток равкен 0 то кратно иначе нет)

--- main.c
+++ main.c
@@ -1,10 +1,12 @@
 #include <stdio.h>
 #include <string.h>
-int main()
-{
-    char s1[1000] = "125";
-    int chislo = 5;
-    size_t len = strlen(s1); // вводим длину s1
+#include <stdbool.h>
+
+int Multipleofanumber(char *s1, int chislo){
+    if (chislo < 0){
+      printf("Делитель должен быть положительныим числом ");
+      return 0;}
+    size_t len = strlen(s1); // объявляем длину s1
     for (int i =len-1 ; i>=0;i--){
         if(s1[i] < 48 || s1[i]> 57){
             printf("Строчка не в десятичной сис");
@@ -13,9 +15,16 @@ int main()
     for (int i = 0; i <(int)len; i++){
         int delim = s1[i] - '0';
         ostat = (10 * ostat + delim) % chislo;}
-    if (ostat == 0){
-      printf("Да число кратно 4 тк остаток равен %d",ostat);}
-    else{printf("Нет число не кратно тк остаток равкен %d",ostat);}
+    return (ostat);}
 
+int main()
+{
+    char s1[1000] = "124";
+    int chislo = 4;
+    if(Multipleofanumber(s1,chislo)==0){
+      printf("Да число кратно 4");}
+
+    else{
+      printf("Нет число не кратно");}
     return 0;
 }
