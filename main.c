57db3c5f73394dbb56340bdb14d56a5921e3f93c
Golovin Kirill <kirill.golovin.256@gmail.com>
2026-03-02 17:43:14 +0700

    улучшение проверки и пара тестов

--- main.c
+++ main.c
@@ -6,6 +6,7 @@
 //АAВB // АB / A�
 //Алфавит русский ас  ������ �����
 // Russian language is a Русский язык sgaeс / Ruin lnu i  Р�кий �з�к
+// Null / sgaeс / Ошибка
 char* Stringcompaction(char *s1, char *s2){
     if(s1 && s2){
     int s3[256] = {0};
@@ -34,7 +35,7 @@ int main()
     char s1[1000] = "Russian language is a Русский язык";
     char s2[1000] = "sgaeс";
     char *nullcheak = NULL;
-    char *res = Stringcompaction(s1,s2);
+    char *res = Stringcompaction(nullcheak,s2);
     if (!res){printf("Ошибка передаем NULL указатель!");}
     else{printf("String s1 with s2 removed: %s\n",s1);}
     return 0;
