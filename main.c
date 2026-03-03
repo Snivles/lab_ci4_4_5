cf87e91181c559c89128e21e7d9df92a06deecaa
Golovin Kirill <kirill.golovin.256@gmail.com>
2026-03-03 21:09:13 +0700

    Изменение и добавление тестов улучшение обработки строки входной и w

--- main.c
+++ main.c
@@ -1,12 +1,34 @@
 #include <stdio.h>
 #include <string.h>
-//s1 = "a ab abc abcd" s2 = abc / a ab abcd
-// s1 = "abc abcd abcd, abcde,abc, a bc adbc. adb" s2 = abc// abcd abcd abcde a bc adbc
-// s1 = "abc" s2 = "abcd" / abc
-// s1 = "Abc abcd abc" s2 = "abc" /
-// s1 = adaf,jjhjabc, abc, , ,abcF. fggh s2= abc / Ошибка
-// s1 = i pass point, sorry s2 = sorry / i pass point
-char* Validation(char *s1){ // Валидация строки
+// abcdefg | cde -> abcdefg с 1 словом отличным от w работает исправно
+// abcdefg. | cde -> abcdefg с 1 словом отличным от w и точкой на конце работает хорошо и работает исправно
+// abcdefg. cdef | cde -> abcdefg с 1 словом словом до точки и 1 после отличными от w работает исправно тк мы не обрабатываем слова за точкой
+// abcdefg. FDS | cde -> abcdefg с 1 словом словом до точки и 1 после отличными (c недопустимыми символами) от w работает исправно тк мы не обрабатываем слова за точкой
+// abcdefg abcdefg | cde -> abcdefg abcdefg с 2 словам отличными от w работает тоже исправно
+
+// cde | cde -> пустая строка тк слово(одно единственное) совпадает с w все хорошо
+
+// "" | cde -> пустая строка тк мы не вводим никакую строку чтобы в ней что-то менять
+// cde | "" -> вся строка cde тк мы ничего не удаляем
+// cde | " ", "," -> Error with string w ошибка тк строка не может содержать разделитель она должна быть словом
+
+
+//a ab abc ab a. a ab abc ab a. | abc -> a ab ab a программа отрабатывает штатно тк идем до точки ( если их несколько до 1 тк дальше уже не смотрим) и удаляем w
+//a ab abc ab a. a ab abc ab a. | abc, / abc" " / " "abc / ,abc -> a ab ab a программа отрабатывает штатно и выводит ошибку тк w не может содержать разделители
+
+
+//FDS . abc | abc -> Error with string s1 работает штатно тк видим не строчную букву и идем к ошибке
+//fds . a abc | abc -> fds все работает верно доходим до точки и заканчиваем проходку
+
+// abc abcd. АБС | abc -> abcd работает штатно убираем первое слово , сохраняем 2 и игнорируем все что дальше (из-за того что стоит точка не кидаем в ошибку ввода )
+// abc abcd АБС | abc -> Error with string s1 работает штатно убираем первое слово , сохраняем 2 и игнорируем все что дальше (из-за того что стоит точка не кидаем в ошибку ввода)
+
+//abcЛ | abc -> Error with string s1 тк появилась кириллица
+// abc | abcЛ -> Error with string w аналогично
+
+// abc,,,,abcd,,,,,,abcde,,,,,,abc | abc -> abcd abcde независимо от кол-во разделителей программа работает штатно
+
+int Compress(char *s1){ // Валидация строки
       int i = 0;
       int flag= 1;
       while (s1[i] != '\0'){
@@ -21,10 +43,22 @@ char* Validation(char *s1){ // Валидация строки
               i++;}
           else{
               s1[0] = '\0';
-              return "\0";}
+              return -1;}
           if (flag==0){
-              return s1;}}
-      return s1;}
+              return 1;}}
+      return 1;}
+
+
+int CleanW(char *w){
+      int i=0;
+      while (w[i] != '\0'){
+          if(w[i] == ' ' || w[i] == ','){
+            return -2;}
+         if (!(w[i] >= 97 && w[i] <= 122)) {
+             return -2;}
+          i++;}
+      return 1;
+}
 
 
 char* WritestringwithoutW(char *s1,char *w,char *news1){
@@ -47,7 +81,7 @@ char* WritestringwithoutW(char *s1,char *w,char *news1){
           if (count != (int)lenw){flag = 0;}
           else{
             for (int j =0; j < count; j++){
-                if (slovo[j]!= w[j] & flag ==1){flag = 0;}}}
+                if (slovo[j]!= w[j] && flag ==1){flag = 0;}}}
           if (flag == 0){
               if (k>0){news1[k] = ' '; k++;}
               for (int h = 0; slovo[h] != '\0'; h++){
@@ -60,17 +94,19 @@ char* WritestringwithoutW(char *s1,char *w,char *news1){
 }
 int main()
 {
-    char s1[1000] = "i pass point sorry. oFrry";// строка в которой пропускаем
-    char w[1000] = "sorry";// символ который пропускаем
+    char s1[1000] = "abc,,,,abcd,,,,,,abcde,,,,,,abc ";// строка в которой пропускаем
+    char w[1000] = "abc";// символ который пропускаем
     char news1[1000] = "";
     char *null_cheak = NULL;
-    Validation(s1);
-    if (s1[0] == '\0'){printf("Error"); return 0;}
+    int rescompresss1 = Compress(s1);
+    int rescleanw = CleanW(w);
+    if (rescompresss1 == -1){printf("Error with string s1"); return 0;}
+    else if(rescleanw== -2){printf("Error with string w"); return 0;}
     char *res = WritestringwithoutW(s1,w,news1);
     if(res){
       printf("Remaining words:%s",news1);
       return 0;}
     else{
-      printf("Error%s",news1);
+      printf("Error with NULL%s",news1);
       return 0;}
 }
