d37f2cbc708f0f38a10b656f9680da276680f5fa
Golovin Kirill <kirill.golovin.256@gmail.com>
2026-03-10 12:02:21 +0700

    Обработка пробелов перед числом и после + обработка оверфлоу

--- main.c
+++ main.c
@@ -19,40 +19,45 @@
 // s1 = "1234&" s2 = 4 ; Error
 // s1 = a s2 = 4 ; Error
 // s1 = 9999999999992 s2 = 4  ; Yes
-
-
-
-int Multipleofanumber(char *s1, int chislo){
+// s1 = 4 s2 = 4 ; YES
+// s1 = 13214124124124 s2 = 4 ; Error
+// s1 = 2147483646 s2 = 4 ; NO
+// s1 =  2147483644 s2 = 4 ; YES
+// s1 = 2147483644 s2 = 4 ; YES
+int Multipleofanumber(char *s1){
     if (!s1){return -3;}
     if (s1[0]=='\0'){return -1;}
     int i = 0;
-    int ostat =0;
-
+    int chislo =0;
+    size_t len = strlen(s1);
     //int flag = 0;
     //if (s1[0] == '-'){
         //flag = 1;}
     //if (s1[0] == '\0' || s1[flag] =='\0'){return -1;}
     //i = flag;
-    while(s1[i]!='\0'){
+    if (s1[i] == ' '){i++;}
+    if (s1[len-1] >= 0 && s1[len-1]==' '){len--;}
+    for (i; i < (int)len ; i++){
         if(s1[i] < 48 || s1[i]> 57){
             return -1;}
         else{
         int delim = s1[i] - '0';
-        ostat = (10 * ostat + delim) % chislo;}
-        i++;}
-    return ostat;
+        if (chislo > (2147483647 - delim) / 10) {
+                return -1; }
+        chislo = (10 * chislo + delim);}}
+    return chislo;
 }
 
 
 
 int main()
 {
-    char s1[1000] = "124";
-    char *null_test;
-    int chislo = 4;
+    char s1[1000] = " 2147483644";
+    char *null_test = NULL;
+    int det = 213123;
 
-    int res = Multipleofanumber(s1,chislo);
-    if(res==0){
+    int res = Multipleofanumber(s1);
+    if(res%det==0){
       printf("Yes");}
     else if(res==-1){
       printf("Error string");}
