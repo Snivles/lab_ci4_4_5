c35794fd207eaf309ffa5025614241a113d62e29
Golovin Kirill <kirill.golovin.256@gmail.com>
2026-03-11 11:08:04 +0700

    разбиение на несколько функций + оптимизация

--- main.c
+++ main.c
@@ -35,24 +35,49 @@
 
 //мусор | abc -> Ошибка code 11
 // abc | мусор -> Ошибка code 11
-
-int Takeaword(char *s1,int i,char *slovo){
-      int count = 0;//счетчик длины нынешнего слова
-      bool cheak_razdel = (s1[i+count] == ',' || s1[i+count] == '.' || s1[i+count] == ' ' || s1[i+count] == '\0');
-      while (!cheak_razdel){
-            if(!(s1[i+count] >= 97 && s1[i+count] <= 122)){
-                return -1;}
+int ProverkaWoda(char *s1, char *w,size_t lenw){
+    if(!(s1&& w)){
+          return -3;}
+    if (s1[0] == '\0' || s1[0] == ' '){
+          return -1;}
+    int i = 0;
+    while (w[i] != '\0'){
+        if ((w[i] >= 65 && w[i] <= 90)){
+          i++;
+          continue;}
+        else if (!(w[i] >= 97 && w[i] <= 122)) {
+          return -2;}
+        i++;}
+    i = 0;
+    while (s1[i] != '\0'){
+        if (s1[i] == '.'){
+            s1[i] = '\0';
+            continue;}
+        else if(s1[i] == ',' || s1[i] == ' '){
+            i++;}
+        else if(!(s1[i] >= 97 && s1[i] <= 122)){
+            return -1;}
+        else{
+        i++;}}
+    return 1; // если w и s1 содержат допустимые символы , то работаем с ними
+}
+int FindLenofWord(char *s1,int i,char *slovo){ // получаем на вход s1(исходная строка) , i - начало нового слова , slovo-само слово которое делаем
+      int count = 0;//счетчик длины нового слова
+      if (s1[i] == '\0') {
+          slovo[0] = '\0';
+          return -4;}
+      bool cheak_razdel = (s1[i+count] == ',' || s1[i+count] == ' ' || s1[i+count] == '\0');
+      while (!cheak_razdel){ // идем пока не встретим разделитель
             slovo[count] = s1[i+count];
             count++;
-            cheak_razdel = (s1[i+count] == ',' || s1[i+count] == '.' || s1[i+count] == ' ' || s1[i+count] == '\0');}
+            cheak_razdel = (s1[i+count] == ',' || s1[i+count] == ' ' || s1[i+count] == '\0');}
       slovo[count] = '\0';// слово отлично от w
-      return count;
+      return count; // возвращаем длину слова которое нашли
 }
 
 
-int SravnenieSlov(char *w, int count,char *slovo){
+int SravnenieSlov(char *w, size_t lenw ,char *slovo,int count){
     int flag = 1;
-    size_t lenw = strlen(w);
     if (count != (int)lenw){
           flag = 0;}// отличны по длине
     else{
@@ -63,68 +88,49 @@ int SravnenieSlov(char *w, int count,char *slovo){
 }
 
 
+int PoiskNachalSlov(char *s1,char *w,int* news1){
+    size_t lenw = strlen(w);
+    int res_compress = ProverkaWoda(s1,w , lenw);
+    if (res_compress!=1){
+        return res_compress;}
 
-int WritestringwithoutW(char *s1,char *w,int* news1){
-    if(!(s1&& w)){
-          return -3;}
-    if (s1[0] == '\0'){
-          return -1;}
     int k = 0;
     int i = 0;
-    while (w[i] != '\0'){
-        if ((w[i] >= 65 && w[i] <= 90)){
+    i = 0;
+    while (s1[i]!='\0'){
+      if (s1[i] == ' ' || s1[i] == ','){
           i++;
           continue;}
-        else if (!(w[i] >= 97 && w[i] <= 122)) {
-          return -2;}
-        i++;}
-    i = 0;
-    bool cheak_razdel = s1[i] == ',' || s1[i] == '.' || s1[i] == ' ' || s1[i] == '\0';
-    bool cheak_simvol = (s1[i] >= 97 && s1[i] <= 122);
-    while (s1[i]!='\0'){
-      if (s1[i]=='.'){
-          s1[i] ='\0';
-          continue;}
-      else if (s1[i] == ' ' || s1[i] == ',') {
-          i++;  // просто пропускаем разделитель
-          continue;}
-      else if(!(s1[i] >= 97 && s1[i] <= 122)){
-          return -1;}
+      if (s1[i] == '\0'){continue;}
       char slovo[1000] = "";
-      int count = Takeaword(s1,i,slovo);
-      if (count == -1){
-          return -1;}
-      int sravnenieslov = SravnenieSlov(w,count,slovo);
+      int count = FindLenofWord(s1,i,slovo);
+      if (count == -4){return -1;}
+      int sravnenieslov = SravnenieSlov(w,lenw,slovo,count);
 
       if (sravnenieslov == 0){
           news1[k] = i;
           k++;}
-      i+= count;
-      cheak_razdel = (s1[i] == ',' || s1[i] == '.' || s1[i] == ' ' || s1[i] == '\0');
-      cheak_simvol = (s1[i] >= 97 && s1[i] <= 122);
-      if (!(cheak_razdel || cheak_simvol) && s1[i] != '\0') {
-          return -1;}
-}
+      i+= count;}
     news1[k] = -1; //конец массива
     return 1;}
 
 int main()
 {
-    char s1[1000] = "fds";// строка в которой пропускаем
-    char w[1000] = "Fasdas";// символ который пропускаем
+    char s1[1000] = "fds . F";// строка в которой пропускаем
+    char w[1000] = "a";// символ который пропускаем
     int news1[1000] = {0};
     char *null_cheak = NULL;
-
-    int res = WritestringwithoutW(s1,w,news1);
-    if(res==1){
-      printf("Remaining words:");
+    int res = PoiskNachalSlov(s1,w,news1);
+    if(res ==1){
+      printf("Remeining words:");
       for(int i = 0; news1[i] != -1 ;i++){
           char slovo[1000] = "";
-          int len = Takeaword(s1,news1[i],slovo);
+          int len = FindLenofWord(s1,news1[i],slovo);
           if (len>0){
-            printf("%s",slovo);}
-      return 0;}}
+            printf("%s ",slovo);}}
+      return 0;}
     else if(res==-3){printf("Error with NULL");}
     else if(res == -2){printf("Error with string w");}
     else if(res==-1){printf("Error with string s1");}
+    return 0;
 }
